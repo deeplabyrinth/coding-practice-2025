ca23b366e554f7b9018c6904b9c15eab897bd5a9
John Q <jquiruz1@gmail.com>
2025-08-17 13:37:39 -0700

    add text block containing story in Answer class

--- java-basics/variables_lab/Answer.java
+++ java-basics/variables_lab/Answer.java
@@ -2,12 +2,12 @@ package variables_lab;
 
 public class Answer {
 
-	public static void main(String[] args) {
+	static String fillInTheBlank(String season, int numberOfCups, String adjective) {
 
-		protected static String fillInTheBlank(String season, int numberOfCups, String adjective) {
-
-			protected String story = "";
-			return story;
-		}
+		// TODO: Add variables to string block to create a story
+		String story = """
+			On a %s %s day, I drink a minimum of %d cups of coffee.
+			""".format(adjective, season, numberOfCups);
+		return story;
 	}
 }
--- java-basics/variables_lab/notes.md
+++ java-basics/variables_lab/notes.md
@@ -1,2 +1,9 @@
 ### Fill in the Blank
 In this challenge, you'll create a fill-in-the-blank story program where the blanks are filled by variables.
+
+#### Challenges I faced
+* Wrongly used the protected access modifiers for Answer methods and fields
+* Main resides within its own class and cannot directly access protected members of a different class
+
+#### Successes
+* Used the .format method to interpolate between variables and placeholders within a text block (Java 15)
