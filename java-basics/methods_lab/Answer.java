3c31323f0deac450eeefaa203cce1c0edae380f3
John Q <jquiruz1@gmail.com>
2025-08-17 18:47:45 -0700

    delete swap files and add methods to methods lab

---
+++ java-basics/methods_lab/Answer.java
@@ -0,0 +1,34 @@
+package methods_lab;
+
+public class Answer {
+
+	static double calculateNumbers(int firstNumber, int secondNumbers) {
+
+		result = add(firstNumber, secondNumber);
+		result = subtract(firstnNumber, secondNumber);
+		result = multiply(firstNumber, secondNumber);
+		result = divide(firstNumber, secondNumber);
+
+		return result;
+	}
+
+	static double add(int num1, int num2) {
+
+		return num1 + num2;
+	}
+
+	static double subtract(int num1, int num2) {
+
+		return num1 - num2;
+	}
+
+	static double multiply(int num1, int num2) {
+
+		return num1 * num2;
+	}
+
+	static double divide(int num1, int num2) {
+
+		return num1 / num2;
+	}
+}
