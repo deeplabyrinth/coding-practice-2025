e38fbd9c3084b71941ac123b55d375dda9740b00
John Q <jquiruz1@gmail.com>
2025-08-17 18:49:22 -0700

    rename original methods lab to "basics" lab

---
+++ java-basics/basics_lab/Main.java
@@ -0,0 +1,16 @@
+package methods_lab;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String answer = "The name of this method is fooBar";
+		String methodName = Answer.getMethodName();
+
+		if (methodName.equals(answer)) {
+			System.out.println("Correct! Answer: " + answer);
+		} else {
+			System.out.println("Uh oh, the answer was incorrect.");
+		}
+	}
+}
