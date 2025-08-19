abbd8db357322f7429e33c4342ed703db6b25212
John Q <jquiruz1@gmail.com>
2025-08-19 11:57:14 -0700

    finish implementing an interface

--- java-basics/interfaces_lab/Main.java
+++ java-basics/interfaces_lab/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 	
-	public static void main(String() args) {
+	public static void main(String[] args) {
 		
 		String name = "golf shirt";
 		double price = 24.99;
 		String size = "large";
 		String color = "grey";
 		
-		Shirt Shirt = Answer.buyShirt(name, price, size, color);
-		System.out.println(String.format("Name: %s\nPrice: %f\nSize: %s\nColor: %s\n", shirt.getName(), shirt.getPrice(), shirt.getSize(), shirt.getColor());
+		Shirt shirt = Answer.buyShirt(name, price, size, color);
+		System.out.println(String.format("Name: %s\nPrice: %f\nSize: %s\nColor: %s\n", shirt.getName(), shirt.getPrice(), shirt.getSize(), shirt.getColor()));
 	}
 }
--- java-basics/interfaces_lab/Shirt.java
+++ java-basics/interfaces_lab/Shirt.java
@@ -6,4 +6,35 @@ public class Shirt implements Product {
 	private String color;
 
 	// TODO: implement this class' methods and abstract methods
+	public Shirt(String name, double price, String size, String color) {
+		setName(name);
+		setPrice(price);
+		setSize(size);
+		setColor(color);
+	}
+
+	public void setName(String name) { // implemented from Product
+		this.name = name;
+	}
+	public void setPrice(double price) { // implemented from Product
+		this.price = price;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public String getSize() {
+		return size;
+	}
+	public String getColor() {
+		return color;
+	}
 }
