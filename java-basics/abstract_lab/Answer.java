c98eda6083ae6ccd2615da6f7cc9f72d9dcc7e18
John Q <jquiruz1@gmail.com>
2025-08-19 16:01:53 -0700

    implement Answer class

--- java-basics/abstract_lab/Answer.java
+++ java-basics/abstract_lab/Answer.java
@@ -1,6 +1,29 @@
 public class Answer {
 
+	static Instrument addFlute() {
+		return new Flute();
+	}
+
+	static Instrument addHorn() {
+		return new Horn();
+	}
+
+	static Instrument addViolin() {
+		return new Violin();
+	}
+
 	static String[] performConcert() {
 
+		var flute = addFlute();
+		var horn = addHorn();
+		var violin = addViolin();
+
+		String[] sounds = new String[3];
+
+		sounds[0] = flute;
+		sounds[1] = horn;
+		sounds[2] = violin;
+
+		return sounds;
 	}
 }
