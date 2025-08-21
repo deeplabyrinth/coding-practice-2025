489039426bf0d1913cce9809fe2c8a9396578d6f
John Q <jquiruz1@gmail.com>
2025-08-21 12:27:45 -0700

    finish menu interface and decision structure

--- java-basics/banking_system_project/Main.java
+++ java-basics/banking_system_project/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		final String testMenu = """
+
 				**********************************
 				TEST MENU
 				**********************************
@@ -17,12 +18,34 @@ public class Main {
 		char menuInput;
 		Scanner console = new Scanner(System.in);
 
-		System.out.print(testMenu);
-		menuInput = console.next().charAt(0);
-
-		System.out.println(menuInput);
-
 		// Decision structure for menu navigation
+		do {
+			System.out.print(testMenu);
+			menuInput = console.next().charAt(0);
 
+			switch (menuInput) {
+				case '0':
+					System.out.println("TEST: Option 0 was selected");
+					break;
+				case '1':
+					System.out.println("TEST: Option 1 was selected");
+					break;
+				case '2':
+					System.out.println("TEST: Option 2 was selected");
+					break;
+				case '3':
+					System.out.println("TEST: Option 3 was selected");
+					break;
+				case 'q':
+					System.out.println("TEST: Testing ended");
+					break;
+				default:
+					System.out.println("ERROR: Incorrect input. Try again");
+					break;
+			}
+
+		} while (menuInput != 'q');
+
+		console.close();
 	}
 }
