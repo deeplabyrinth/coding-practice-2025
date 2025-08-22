5765593d9a5c3f6897efe2f38ed8fad4bbfd54f1
John Q <jquiruz1@gmail.com>
2025-08-22 12:39:31 -0700

    complete command interface in main

--- java-basics/car_reservation_project/Main.java
+++ java-basics/car_reservation_project/Main.java
@@ -1,7 +1,50 @@
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
+		/* This application allows the user to access a car reservation service via a command
+		 * interface where you can show rental availabily and reserve a selected car. The
+		 * CarReservationService class is a controller that contains the filtering and
+		 * validation of available cars. The service filters by ID (allows user to select car 
+		 * that they want to reserve) and type (type of car for organization when viewing the 
+		 * inventory). Each Car has attributes based on its model, make, type, color, price, 
+		 * and whether or not it is reserved. The only action to perform on a Car is "reserve".
+		 */
 
-		System.out.println("Hello from Car Reservation Services");
+		//TODO: Create car reservation commands
+		final String commandInterface = """
+
+			==============================
+			Welcome to Car Reservation
+			==============================
+			[0] Show availability
+			[1] Reserve car
+
+			Select an option [q/quit]\s""";
+
+		Scanner console = new Scanner(System.in);
+
+		char command;
+		do {
+			System.out.print(commandInterface);
+			command = console.next().charAt(0);
+
+			switch (command) {
+				case '0':
+					System.out.println("Option 0 selected");
+					//CarReservationService.show();
+					break;
+				case '1':
+					System.out.println("Option 1 selected");
+					//CarReservationService.reserve();
+					break;
+				case 'q':
+					System.out.println("Application terminated");
+					break;
+				default:
+					System.out.println("Incorrect command, try again");
+			}
+		} while (command != 'q');
 	}
 }
