e95a5e93df0471e4220e9c43a2c7f84da036acb9
John Quiruz <jquiruz1@gmail.com>
2025-08-24 23:40:25 -0700

    create a ModArrayList that inherits from ArrayList to provide custom method

--- java-basics/inherit_arraylist_lab/ModArrayList.java
+++ java-basics/inherit_arraylist_lab/ModArrayList.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class ModArrayList<D> extends ArrayList<D> {
-    /* Inherit from the super class ArrayList, The list we create can allow any type of data to be in the list
+    /* We want to create a fool-proof way of accessing items in a given list, we want to create a getUsingMod method
+     * that takes an index and retrieves an item from the list at that index. If the index is invalid, then the mod and
+     * absolute value operations should be used to make the index positive and mod the index based on the length of the
+     * list.
+
+     * Inherit from the super class ArrayList, The list we create can allow any type of data to be in the list
      * denoted by 'D'. Our ModArrayList can also store any data as indicated by 'D'.
      */
     public D getUsingMod(int index) {
