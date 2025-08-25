f1ac32ab0b761525b21692e13ecbcac8c31d94c6
John Quiruz <jquiruz1@gmail.com>
2025-08-24 23:40:19 -0700

    create a ModArrayList that inherits from ArrayList to provide custom method

--- java-basics/inherit_arraylist_lab/ModArrayList.java
+++ java-basics/inherit_arraylist_lab/ModArrayList.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
-public class ModArrayList extends ArrayList {
-
+public class ModArrayList<D> extends ArrayList<D> {
+    /* Inherit from the super class ArrayList, The list we create can allow any type of data to be in the list
+     * denoted by 'D'. Our ModArrayList can also store any data as indicated by 'D'.
+     */
+    public D getUsingMod(int index) {
+        index = Math.abs(index % this.size());
+        return this.get(index);
+    }
 }
