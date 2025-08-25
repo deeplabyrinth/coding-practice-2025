744363299eda3ec8d99d0869d5ad5698434bbf62
John Q <jquiruz1@gmail.com>
2025-08-24 21:59:16 -0700

    implement employee attributes and inheret from person its attributes

--- java-basics/inheritance_exercise/Employee.java
+++ java-basics/inheritance_exercise/Employee.java
@@ -1,8 +1,20 @@
 package inheritance_exercise;
 
-public class Employee {
+public class Employee extends Person {
     //TODO: Establish generalized employee attributes to be inherited
     // by salesperson and analyst classes
+    private double salary;
 
+    public Employee(String name, int age, double salary) {
+        super(name, age);
+        setSalary(salary);
+    }
 
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
