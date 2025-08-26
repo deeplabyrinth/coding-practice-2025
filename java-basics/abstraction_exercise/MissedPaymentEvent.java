221a21e961e52c8806e2c8f9005702a5fbbde23e
John Quiruz <jquiruz1@gmail.com>
2025-08-25 17:56:40 -0700

    implement missed payment event class

---
+++ java-basics/abstraction_exercise/MissedPaymentEvent.java
@@ -0,0 +1,20 @@
+import java.sql.Timestamp;
+
+public class MissedPaymentEvent implements Event {
+    private Long createdTimeStamp;
+    private String id;
+
+    public AccountTransferEvent(String id) {
+        this.createdTimeStamp = new Timestamp(System
+                .currentTimeMillis()).getTime();
+        this.id = id;
+    }
+
+    @Override
+    public Long getTimeStamp() { return this.createdTimeStamp; }
+
+    @Override
+    public void process() {
+        System.out.println("Customer " + id + " missed their payment. " +
+                "Sending a bill to the customer.");
+}
