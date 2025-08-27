855e23848d6d40703c343700e4c4e114566bd22d
John Quiruz <jquiruz1@gmail.com>
2025-08-27 14:24:00 -0700

    complete arrays lab

--- java-data-structures/arrays_lab/Main.java
+++ java-data-structures/arrays_lab/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		// Increment each number in the 'numbers' array by one
+		int[] numbers = new int[] {1, 2, 3, 4, 5};
+		int[] result = incrementArray(numbers);
+		System.out.println(Arrays.toString(numbers));
+	}
+
+	static int[] incrementArray(int[] numbers) {
+		for (int i = 0; i < numbers.length; ++i) {
+			numbers[i]++;
+		}
+
+		// Using stream operators
+		//return Arrays.stream(numbers)
+			//.map(number -> number + 1)
+			//.toArray();
+
+		return numbers;
 	}
 }
