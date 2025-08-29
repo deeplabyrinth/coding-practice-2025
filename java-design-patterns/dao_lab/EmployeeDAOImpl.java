9bef3cd9b1bd9c471b77dc790a0cf140cbb3ab8f
John Quiruz <jquiruz1@gmail.com>
2025-08-28 18:01:26 -0700

    implement DAO methods

--- java-design-patterns/dao_lab/EmployeeDAOImpl.java
+++ java-design-patterns/dao_lab/EmployeeDAOImpl.java
@@ -1,3 +1,35 @@
-public class EmployeeDAOImpl extends EmployeeDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Override
+    public Employee get(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAll() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public int save(Employee employee) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int insert(Employee employee) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int update(Employee employee) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(Employee employee) throws SQLException {
+        return 0;
+    }
 }
