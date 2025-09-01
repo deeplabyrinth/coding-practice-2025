20754ea05cd8e1e696a3f9ff527891966f4ea724
John Q <jquiruz1@gmail.com>
2025-08-31 17:48:32 -0700

    create and implement DatabaseUtils

---
+++ java-design-patterns/jdbc_advanced_lab/src/main/java/data/util/DatabaseUtils.java
@@ -0,0 +1,35 @@
+package data.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class DatabaseUtils {
+    private static final String URL = "jdbc:mariadb://localhost:3306/bookstore";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+    private static final Logger LOGGER = Logger.getLogger(DatabaseUtils.class.getName());
+    private static final String exceptionFormat = "exception in %s, message: %s, code: %s";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            synchronized (DatabaseUtils.class) {
+                if (connection == null) {
+                    try {
+                        connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                    } catch (SQLException e) {
+                        handleSQLException("DatabaseUtils.getConnection", e, LOGGER);
+                    }
+                }
+            }
+        }
+        return connection;
+    }
+
+    public static void handleSQLException(String method, SQLException e, Logger log) {
+        log.warning(String.format(exceptionFormat, method, e.getMessage(), e.getErrorCode()));
+        throw new RuntimeException(e);
+    }
+}
